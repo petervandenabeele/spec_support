47e89aaaf33885434ed6387616f6e4e8922070c5
Peter Vandenabeele <peter@vandenabeele.com>
2011-12-16 17:26:38 +0100

    spec/support directory exists

--- test/spec_support_test.rb
+++ test/spec_support_test.rb
@@ -4,4 +4,8 @@ class SpecSupportTest < ActiveSupport::TestCase
   test "truth" do
     assert_kind_of Module, SpecSupport
   end
+
+  test "spec/support directory exists" do
+    assert File.exist?('spec/support'), "spec/support directory should exist"
+  end
 end
