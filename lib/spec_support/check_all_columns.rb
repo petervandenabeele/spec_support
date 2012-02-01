d8a1b3bfb45b6a05d9294b5a7b7e95d1a3e20978
Peter Vandenabeele <peter@vandenabeele.com>
2012-02-01 18:26:15 +0100

    Remove the 'it' so 'it' can be placed higher up to catch interrupts

--- lib/spec_support/check_all_columns.rb
+++ lib/spec_support/check_all_columns.rb
@@ -1,7 +1,5 @@
 def check_all_columns(instance)
   instance.class.columns.each do |column|
-    it "has data in #{column.name} column" do
-      instance.send(:"#{column.name}").should_not be_nil
-    end
+    instance.send(:"#{column.name}").should_not be_nil
   end
 end
