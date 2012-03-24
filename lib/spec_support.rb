4576dc25fcefb4d7ecde0eb5b2a1f15ac2d86660
Peter Vandenabeele <peter@vandenabeele.com>
2012-03-24 23:30:43 +0100

    Use proper LOAD_PATH so we can avoid require_relative for Jruby

--- lib/spec_support.rb
+++ lib/spec_support.rb
@@ -1,6 +1,10 @@
 module SpecSupport
 
-  require_relative './spec_support/check_all_columns'
-  require_relative './spec_support/delete_nil_values'
+  spec_support_path = File.join(File.expand_path(File.dirname(__FILE__)),'spec_support')
+
+  $LOAD_PATH.unshift(spec_support_path)
+
+  require 'check_all_columns'
+  require 'delete_nil_values'
 
 end
