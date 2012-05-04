c68c0b837646f108013ce7a9f0a1d32568b240b4
Peter Vandenabeele <peter@vandenabeele.com>
2012-05-04 17:40:55 +0200

    Add a warning on empty column in check_all_columns

--- lib/spec_support/check_all_columns.rb
+++ lib/spec_support/check_all_columns.rb
@@ -1,6 +1,8 @@
 # FIXME scope this to an RSpec sub module
 def check_all_columns(instance)
   instance.class.columns.each do |column|
-    instance.send(:"#{column.name}").should_not be_nil
+    val = instance.send(:"#{column.name}")
+    puts "\n WARNING: empty column for #{column.name}" if val.nil?
+    val.should_not be_nil
   end
 end
